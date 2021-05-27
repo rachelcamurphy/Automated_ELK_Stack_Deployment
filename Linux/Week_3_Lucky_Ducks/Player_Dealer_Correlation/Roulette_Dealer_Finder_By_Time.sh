4e292584ba3a89cd5ab548e9a906b2c469eb8070
Rachel Murphy <rachelcamurphy@gmail.com>
2021-05-27 12:01:50 -0500

    linux update

--- Linux/Week_3_Lucky_Ducks/Player_Dealer_Correlation/Roulette_Dealer_Finder_By_Time.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/sh
-#Rachel Murphy 
-#Homework 3 Homework : A High Stakes Investigation
-#This script will accept arguments for date, time, am/pm, and output the Roulette Dealer that was working.
-
-cat DEALER_SCHEDULE_FINAL | awk -F " " '{print $1, $2, $3, $4, $5, $6}' | grep "$2" 
